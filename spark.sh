50f0aae90279ffead687e4d7d0be46a56474a8dd
Liel Bach <ybach123@gmail.com>
2021-03-15 19:31:37 +0200

    Adds spark worker script

--- spark.sh
+++ spark.sh
@@ -1,5 +1,11 @@
 pushd ~
 
 curl https://archive.apache.org/dist/spark/spark-2.4.3/spark-2.4.3-bin-without-hadoop.tgz --output ~/spark-2.4.3.tgz
+tar -xf spark-2.4.3.tgz
+sudo mv spark-2.4.3-bin-without-hadoop /usr/local/spark
+echo export SPARK_DIST_CLASSPATH=$(hadoop classpath) | tee -a /usr/local/spark/conf/spark_env.sh
+echo export YARN_CONF_DIR=/usr/local/hadoop/etc/hadoop | tee -a /usr/local/spark/conf/spark_env.sh
+echo export YARN_CONF_DIR=/usr/local/hadoop/etc/hadoop  | sudo tee -a /etc/profile.d/spark_env.sh
+echo PATH="$PATH:/usr/local/spark/bin" | sudo tee /etc/environment
 
 popd
